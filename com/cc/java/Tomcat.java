3cc07c51f186da5e96e049802b124071b554e2b5
jy4education_git <jy4education@gmail.com>
2026-01-28 10:56:14 +0100

    add Tomcat

--- com/cc/java/Tomcat.java
+++ com/cc/java/Tomcat.java
@@ -1,19 +1,15 @@
 package com.cc.java;
 
-public class Cat {
+public class Tomcat {
 
   private String firstName;
   private String furColor;
   private int age;
-  private boolean isFemale; 
-
-  private int counter;
-
-  public Cat(String firstName, String furColor, int age, boolean isFemale) {
+    
+  public Tomcat(String firstName, String furColor, int age) {
     this.firstName = firstName;
     this.furColor = furColor;
     this.age = age;
-    this.isFemale = isFemale;
   }
 
   public String getStringAttributes(String op) {
@@ -27,31 +23,11 @@ public class Cat {
     }
   }
 
-  public String getAge() {
-    if (isFemale) {
-      return checkCompliance();
-    } else {
-      return String.valueOf(age);
+  public String getAge() {  
+     return String.valueOf(age);
     }
   }
 
-private String checkCompliance(){
-
-         counter++;
-
-        switch (counter) {
-            case 1:
-                return "This is an inappropriate question!"; // 1st time
-            case 2:
-                return "I've told you once!"; // 2nd time
-            case 3:
-                return "Talk to the hand!"; // 3rd time 
-            default:
-                return "1#!?&&%"; //4th ++
-        }
-}
 
 
-
-}
   
